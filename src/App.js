d366fa0ccdbf1013a9f78adca72afc27ec3c58ec
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2022-06-16 09:55:40 +0300

    update: fetching data from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,64 +1,64 @@
 import "./App.css";
-import { Navbar } from "./components/Navbar";
-import { Sidebar } from "./components/Sidebar";
-import { Breadcrumb } from "./components/Breadcrumb";
-import { EmployeeList } from "./components/List";
-import { Footer } from "./components/Footer";
-import { Card } from "./components/Card";
-import { faker } from "@faker-js/faker";
-import { useState } from "react";
+import {Navbar} from "./components/Navbar";
+import {Sidebar} from "./components/Sidebar";
+import {Breadcrumb} from "./components/Breadcrumb";
+import {UserList} from "./components/List";
+import {Footer} from "./components/Footer";
+import {Card} from "./components/Card";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
-  const employees = new Array(15).fill(null).map((_) => ({
-    name: faker.name.findName(),
-    position: faker.company.bsNoun(),
-    office: faker.address.cityName(),
-    age: faker.random.numeric(2),
-    startDate: new Date().toISOString().split("T")[0],
-    salary: faker.random.numeric(6),
-  }));
+    let [users, setUsers] = useState([]);
 
-  const [sidebarClass, setSidebarClass] = useState("sb-nav-fixed");
+    const [sidebarClass, setSidebarClass] = useState("sb-nav-fixed");
 
-  function toggleSidebarClass() {
-    setSidebarClass(
-      sidebarClass.includes("toggled")
-        ? "sb-nav-fixed"
-        : "sb-nav-fixed sb-sidenav-toggled"
-    );
-  }
+    useEffect(async () => {
+        axios.get('https://jsonplaceholder.typicode.com/users').then(result => {
+            const {data} = result;
+            setUsers(data);
+        });
+    }, []);
 
-  return (
-    <div className={sidebarClass}>
-      <Navbar toggleSidebarClass={toggleSidebarClass} />
-      <div id="layoutSidenav">
-        <div id="layoutSidenav_nav">
-          <Sidebar />
-        </div>
-        <div id="layoutSidenav_content">
-          <main>
-            <div className="container-fluid px-4">
-              <h1 className="mt-4">Tables</h1>
-              <Breadcrumb />
-              <Card>
-                DataTables is a third party plugin that is used to generate the
-                demo table below. For more information about DataTables, please
-                visit the
-                <a target="_blank" href="https://datatables.net/">
-                  official DataTables documentation
-                </a>
-                .
-              </Card>
-              <Card title="DataTable Example">
-                <EmployeeList items={employees} />
-              </Card>
+    function toggleSidebarClass() {
+        setSidebarClass(
+            sidebarClass.includes("toggled")
+                ? "sb-nav-fixed"
+                : "sb-nav-fixed sb-sidenav-toggled"
+        );
+    }
+
+    return (
+        <div className={sidebarClass}>
+            <Navbar toggleSidebarClass={toggleSidebarClass}/>
+            <div id="layoutSidenav">
+                <div id="layoutSidenav_nav">
+                    <Sidebar/>
+                </div>
+                <div id="layoutSidenav_content">
+                    <main>
+                        <div className="container-fluid px-4">
+                            <h1 className="mt-4">Tables</h1>
+                            <Breadcrumb/>
+                            <Card>
+                                DataTables is a third party plugin that is used to generate the
+                                demo table below. For more information about DataTables, please
+                                visit the
+                                <a target="_blank" href="https://datatables.net/">
+                                    official DataTables documentation
+                                </a>
+                                .
+                            </Card>
+                            <Card title="DataTable Example">
+                                <UserList items={users}/>
+                            </Card>
+                        </div>
+                    </main>
+                    <Footer/>
+                </div>
             </div>
-          </main>
-          <Footer />
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
